9046004e74c1b7fde49a4df9c219e1945ff4595a
Kirill <133701317+kunaesv@users.noreply.github.com>
2024-09-24 10:11:42 +0300

    Add files via upload

---
+++ zz4/src/Main.java
@@ -0,0 +1,116 @@
+public class Main {
+    public static void main(String[] args) {
+        ModelQ testCar = new ModelQ();
+
+        System.out.println("Характеристики модели:");
+        System.out.println("Ускорение: " + testCar.acceleration + " км/(чс)");
+        System.out.println("Максимальная скорость: " + testCar.maxSpeed + " км/ч");
+
+        System.out.println("\nНачало теста!");
+
+        System.out.println("Едем на автопилоте:");
+        for (int second = 0; second < 5; second++) {
+            testCar.accelerateByAutopilot();
+        }
+        System.out.print("Скорость Q спустя 5с на автопилоте: " + testCar.speed + " км/ч");
+        checkResult(55.0, testCar.speed);
+
+        for (int second = 0; second < 5; second++) {
+            testCar.accelerateByAutopilot();
+        }
+        System.out.print("Скорость Q спустя ещё 5с на автопилоте: " + testCar.speed + " км/ч");
+        checkResult(60.0, testCar.speed);
+
+
+        System.out.println("Переходим в ручной режим:");
+        for (int second = 0; second < 2; second++) {
+            testCar.accelerate();
+        }
+        System.out.print("Скорость Q спустя 2с в ручном режиме: " + testCar.speed + " км/ч");
+        checkResult(260.0, testCar.speed);
+
+        for (int second = 0; second < 2; second++) {
+            testCar.accelerate();
+        }
+        System.out.print("Скорость Q спустя ещё 2с в ручном режиме: " + testCar.speed + " км/ч");
+        checkResult(300.0, testCar.speed);
+
+
+        System.out.println("Проверяем торможение:");
+        int brakingTime = 0;
+        while (testCar.speed > 0) {
+            testCar.brake();
+            brakingTime++;
+        }
+        System.out.print("Время торможения до полной остановки: " + brakingTime + "c");
+        checkResult(3, brakingTime);
+        System.out.print("Скорость Q: " + testCar.speed + " км/ч");
+        checkResult(0.0, testCar.speed);
+    }
+
+    private static void checkResult(double expect, double actual) {
+        if (expect == actual) {
+            System.out.println(" ✅");
+        } else {
+            System.out.println(" ❌");
+        }
+    }
+
+    private static void checkResult(int expect, int actual) {
+        if (expect == actual) {
+            System.out.println(" ✅");
+        } else {
+            System.out.println(" ❌");
+        }
+    }
+}
+class Texla {
+    protected double speed;
+    protected double maxSpeed;
+    protected double acceleration;
+    protected double brakingSpeed;
+    protected double autoPilotMaxSpeed;
+    protected double autoPilotAcceleration;
+
+    public Texla() {
+        acceleration = 30;
+        brakingSpeed = 120;
+        autoPilotMaxSpeed = 60;
+        autoPilotAcceleration = 11;
+    }
+
+    public void accelerateByAutopilot() {
+        if (speed < autoPilotMaxSpeed) {
+            speed += autoPilotAcceleration;
+        } else {
+            speed = autoPilotMaxSpeed;
+        }
+    }
+
+
+    public void accelerate() {
+        if (speed < maxSpeed) {
+            speed += acceleration;
+        } else {
+            speed = maxSpeed;
+        }
+    }
+
+    public void brake() {
+        if (speed > 0) {
+            speed -= brakingSpeed;
+        } else {
+            speed = 0;
+        }
+    }
+}
+
+class ModelQ extends Texla {
+
+    public ModelQ() {
+        speed = 0;
+        maxSpeed = 300;
+        acceleration = 100;
+        brakingSpeed = 120;
+    }
+}
